83ada6add5ef82c869fb30ccf0486409005ae72b
mrtrandinhvn <mrtrandinhvn@gmail.com>
2016-09-20 17:51:31 +0700

    Initialization

---
+++ src/JAVIdolsDetector/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JAVIdolsDetector.Models;
+using Microsoft.Extensions.Options;
+
+namespace JAVIdolsDetector.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationSettings _settings;
+
+        public HomeController(IOptions<ApplicationSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/JAVIdolsDetector/Models/ApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JAVIdolsDetector.Models
+{
+    public class ApplicationSettings
+    {
+        public string ApiKey { get; set; }
+    }
+}
