fb1e72cbaa6ce1b4a86a87a0840fa00d5fd109c6
mrtrandinhvn <mrtrandinhvn@gmail.com>
2016-10-04 18:23:15 +0700

    Practicing React

--- src/JAVIdolsDetector/wwwroot/js/ReactTraining/thinking.jsx
+++ src/JAVIdolsDetector/wwwroot/js/ReactTraining/thinking.jsx
@@ -1,6 +1,10 @@
 ﻿var App = React.createClass({
     render: function () {
-        return (<FilterableProductTabe apiUrl="http://localhost:3000/api/products" />);
+        return (
+        <div>
+    <h3>Filterable Product table</h3>
+    <FilterableProductTabe apiUrl="http://localhost:3000/api/products"></FilterableProductTabe>
+        </div>);
     }
 });
 
@@ -8,49 +12,74 @@ var FilterableProductTabe = React.createClass({
     getInitialState: function () {
         return {
             categories: [],
-            filterText: "",
+            filter: "",
             inStockOnly: false
         }
     },
-    loadData: function () {
-        function indexOf(arr, field, fieldValue) {
-            var result = [];
-            for (var i = 0; i < arr.length; i++) {
-                if (arr[i][field] == fieldValue) {
-                    result.push(i);
-                }
+    indexOf: function (arr, field, fieldValue) {
+        var result = [];
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i][field] == fieldValue) {
+                result.push(i);
             }
-            return result;
         }
-        function groupByField(data, field) {
-            // requirement: data is sorted by field
-            var result = [];
-            var lastGroup = null;
-            for (var i = 0; i < data.length; i++) {
-                var group = {
-                    name: data[i][field],
-                    data: []
-                };
-                var indexes = indexOf(result, "name", group.name);
-                if (indexes.length == 1) { // group should be unique in result array
-                    result[indexes[0]].data.push(data[i]);
-                } else {
-                    // add new group into result
-                    group.data.push(data[i]);
-                    result.push(group);
-                }
+        return result;
+    },
+    groupByField: function (data, field) {
+        // requirement: data is sorted by field
+        var result = [];
+        var lastGroup = null;
+        for (var i = 0; i < data.length; i++) {
+            var group = {
+                name: data[i][field],
+                data: []
+            };
+            var indexes = this.indexOf(result, "name", group.name);
+            if (indexes.length == 1) { // group should be unique in result array
+                result[indexes[0]].data.push(data[i]);
+            } else {
+                // add new group into result
+                group.data.push(data[i]);
+                result.push(group);
             }
-            return result;
-        };
+        }
+        return result;
+    },
+    loadData: function (filter, isStocked) {
         $.ajax({
             url: this.props.apiUrl,
             data: null,
             dataType: "json",
             type: "GET",
             cache: false,
-            success: function (data) {
-                if (!data) { return; }
-                var categories = groupByField(data, "category");
+            success: function (list) {
+                if (!list) { return; }
+                var data = [];
+                list.forEach(function (product, index) {
+                    if (!filter && !isStocked) {
+                        // no filter, no stock only
+                        data.push(product);
+                    } else if (!filter && isStocked) {
+                        // no filter, stock only
+                        if (product.isStocked) {
+                            data.push(product);
+                        }
+                    } else {
+                        // has filter
+                        if (!isStocked) {
+                            // has filter, no stock only
+                            if (product.name.toLowerCase().includes(filter.toLowerCase())) {
+                                data.push(product);
+                            }
+                        } else {
+                            // has filter, has stock only
+                            if (product.stocked && product.name.toLowerCase().includes(filter.toLowerCase())) {
+                                data.push(product);
+                            }
+                        }
+                    }
+                });
+                var categories = this.groupByField(data, "category");
                 this.setState({ categories: categories });
             }.bind(this),
             error: function (xhr, status, err) {
@@ -61,18 +90,31 @@ var FilterableProductTabe = React.createClass({
     componentDidMount: function () {
         this.loadData();
     },
+    onSearch: function (filter, isStocked) {
+        this.loadData();
+    },
     render: function () {
         return (
         <div>
-            <SearchBar></SearchBar>
+            <SearchBar onSearch={this.onSearch}></SearchBar>
             <ProductTable categories={this.state.categories}></ProductTable>
         </div>
         );
     }
 });
 var SearchBar = React.createClass({
+    onSearchChanged: function (event) {
+        var searchText = this.refs.searchText.value;
+        var isStocked = this.refs.stockCheckbox.checked;
+        this.props.onSearch(searchText, isStocked);
+    },
     render: function () {
-        return null;
+        return (
+            <div className="search-bar">
+                <input type="search" className="form-control" ref="searchText" placeholder="Search here..." onChange={this.onSearchChanged} />
+                <div className="checkbox"><label><input type="checkbox" ref="stockCheckbox" className="" onChange={this.onSearchChanged} /> Stocked only</label></div>
+            </div>
+            );
     }
 });
 var ProductTable = React.createClass({
