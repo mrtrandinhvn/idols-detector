7f9ddcf8d7ba9e6e324265f7159282ce2ee1601d
mrtrandinhvn <mrtrandinhvn@gmail.com>
2016-10-12 11:53:38 +0700

    Remove wwwroot/lib/ folder because we're using bower to manage client-side lib

--- src/JAVIdolsDetector/wwwroot/_references.js
+++ src/JAVIdolsDetector/wwwroot/_references.js
@@ -2,7 +2,3 @@
 /// <reference path="js/lib/react/browser.min.js" />
 /// <reference path="js/lib/react/react.js" />
 /// <reference path="js/lib/react/react-dom.js" />
-/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
